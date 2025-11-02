a990c44084cc5127f3d1e21841b218689ea697b3
CHAMPION <championjean@live.fr>
2025-11-02 12:04:32 +0100

    config

--- softdesk/projet/models.py
+++ softdesk/projet/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from rest_framework import serializers
+
 
 # Utilisateurs
 class User(AbstractUser):
     age = models.PositiveIntegerField()
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'age', 'can_be_contacted', 'can_data_be_shared', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Assure le hashage sécurisé du mot de passe
+        user.save()
+        return user
--- softdesk/projet/views.py
+++ softdesk/projet/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
